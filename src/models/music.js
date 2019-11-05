1b1d99f6ecb862d879f5d4da0ef789982d20602c
thiagomartinssaraiva <thiagomartinssaraiva@gmail.com>
2019-11-04 23:35:43 -0300

    creating the music model

---
+++ src/models/music.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { name: 'Drown', bandName: 'Bring Me The Horizon', albumName: 'Thats the Spirit' },
+  { name: 'Throne', bandName: 'Bring Me The Horizon', albumName: 'Thats the Spirit' },
+  { name: 'Avalanche', bandName: 'Bring Me The Horizon', albumName: 'Thats the Spirit' },
+  { name: 'Run', bandName: 'Bring Me The Horizon', albumName: 'Thats the Spirit' }
+]
