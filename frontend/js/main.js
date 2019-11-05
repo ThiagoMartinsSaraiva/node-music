c36a2bfa2988544b98d78295f1c9f063e4f2b4a1
thiagomartinssaraiva <thiagomartinssaraiva@gmail.com>
2019-11-05 19:40:44 -0300

    showing music that fits in the filter

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -12,10 +12,12 @@ const getAllMusics = () => {
 
 const getMusicByName = () => {
   let musicName = inputMusicName.value
+  let musicList = []
   fetch('http://localhost:3000/music', {
     headers: { name: musicName }
   })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => data.forEach(music => musicList.push(`<p>${music.name}</p>`)))
+    .then(() => app.innerHTML = (musicList.join('')))
     .catch(err => console.log(err))
 }
